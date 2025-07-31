e488e9d0ecb29ee480947d2cd13f136726921286
Vanessa Maher <vanessa.maher@keyin.com>
2025-07-31 20:56:12 -0230

    Initial commit

---
+++ src/main/java/com/example/binarytree/BinaryTreeApp.java
@@ -0,0 +1,11 @@
+package com.example.binarytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryTreeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryTreeApp.class, args);
+    }
+}
